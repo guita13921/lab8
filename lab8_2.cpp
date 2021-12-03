a4a27a3fa66778c370188180191f45b0fbea29eb
guita13921 <guita13921@gmail.com>
2021-12-03 16:55:57 +0700

    lab8_2.cpp completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,5 +1,39 @@
 //Dialog of the first example is given below.
 
+#include <iostream>
+using namespace std;
+
+string name;
+int ID;
+string movie;
+string day;
+string sentence;
+
+int main(){
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << "\n";
+    cout << "?????: ";
+    getline(cin,name);
+    cout <<  "Fahsai: Wow!!! " << name << " is a really cool name." << "\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << "\n";
+    cout << name << ": ";
+    cin >> ID;
+    cout << "Fahsai: I think you may be GEAR " <<  (ID/10000000)-12 << ". I have a free movie ticket for you."<< "\n";
+    cout << "Fahsai: Let's go to the cinema together!!!"<< "\n";
+    cout << "Fahsai: What movie do you want to watch?" << "\n";
+    cin.ignore();
+    cout << name << ": ";
+    getline(cin,movie);
+    cout << "Fahsai: So....which day are you free to go with me?" << "\n";
+    cout << name << ": ";
+    getline(cin,day);
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you." << "\n";
+    cout << name << ": ";
+    getline(cin,sentence); 
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+
+
+return 0;
+}
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
 ?????: Luke Skywalker
